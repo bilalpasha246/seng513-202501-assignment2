405a8840bfb42272bb68a28f38ade7fc8d86864a
yousefh10 <101438168+yousefh10@users.noreply.github.com>
2025-03-04 16:19:47 -0700

    basic website setup

--- quiz-site/js/main.js
+++
@@ -1,3 +0,0 @@
-// Example of ES6 import/export
-import { greeting } from './module.js';
-console.log(greeting('Developer'));
---
+++ script.js
@@ -0,0 +1,3 @@
+async function fetch(){
+    response = async_fetch("https://opentdb.com/api.php?amount=30&category=9&difficulty=easy&type=multiple");
+}
