7f576e72ad1b697da77ca93c47d3ae1186ae8c18
bilalpasha246 <94236236+bilalpasha246@users.noreply.github.com>
2025-03-12 12:45:07 -0600

    uploading for transition

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { Quiz } from "./quiz.js";
+import { User } from "./user.js"
 
 document.addEventListener("DOMContentLoaded", async () => {
   const submitButton = document.querySelector(".submit");
@@ -12,33 +13,28 @@ document.addEventListener("DOMContentLoaded", async () => {
       }
     })
   }
-
 if(window.location.pathname.includes("index.html")){
   const username = localStorage.getItem("username"); // Retrieve username
   console.log(username); // This will now correctly display the username  
   const quiz = new Quiz();
-  quiz.score = 0;  // Reset score when starting a new game
-  quiz.incorrect = false; // Reset incorrect flag
+  await new Promise(resolve => setTimeout(resolve, 3000));
   await quiz.fetchQuestions();
   document.querySelector(".next").addEventListener("click", async () => {
-    if(quiz.incorrect == false){
-      if(quiz.score%5 == 0 && quiz.score != 0){
-        await quiz.fetchQuestions();
-      }
-      if(quiz.score == 15){
-        localStorage.setItem("quiz_score", quiz.score);
-        window.location.replace("game-over.html");
-      }
-      quiz.displayQuestion();
+    if(quiz.score%5 == 0 && quiz.score != 0){
+      await quiz.fetchQuestions();
     }
-    else{
+    if(quiz.counter == 16){
       localStorage.setItem("quiz_score", quiz.score);
       window.location.replace("game-over.html");
     }
+    quiz.displayQuestion();
   });
 }
 
 if(window.location.pathname.includes("game-over.html")){
+  const user = new User()
+  const userData = { username: username, score: quiz_score };
+  const message = user.finalMessage
   document.querySelector(".retry").addEventListener("click", async () => {
     window.location.replace("index.html");
   });
--- user.js
+++ user.js
@@ -1,8 +1,37 @@
 export class User {
     constructor(){
-        this.username = null
+        this.username = "default"
         this.high_scores = [];
     }
+
+    sortFunction(a, b) {
+        if (a[0] === b[0]) {
+            return 0;
+        }
+        else {
+            return (a[0] < b[0]) ? -1 : 1;
+        }
+    }
+
+    finalMessage(){
+        return(`Congrats ${this.username}, your score on this run was ${this.score}/15`)
+    }
     
+
+    addHighScore(difficulty, score){
+        for(let i = 4; i >= 0; i--){
+            if(this.high_scores.length < 5){
+                this.high_scores.push(difficulty, score);
+                high_scores.sort(this.sortFunction);
+                break;
+            }
+            else if(i <= this.high_scores.length()-1){
+                if(this.high_scores[i][1] <  score){
+                    this.high_scores[0][1] = score;
+                    this.high_scores.sort(this.sortFunction);
+                }
+            }
+        }
+    }
     
 }
