ac12869443cfb46ad6fae23bb958ef8daca01aac
bilalpasha246 <94236236+bilalpasha246@users.noreply.github.com>
2025-03-13 17:08:36 -0600

    fixed highscore ranking
    was comparing strings rather than integers

--- user.js
+++ user.js
@@ -3,14 +3,20 @@ export class User {
         this.high_scores = JSON.parse(sessionStorage.getItem("high_scores")) || [];
     }
 
-    sortFunction(a, b) {
-        if (a[0] === b[0]) {
-            return 0;
+    sortFunction(twodarr) {
+        for(let i = 0; i < twodarr.length; i++){
+            for(let j = i+1; j < twodarr.length; j++){
+                if(Number(twodarr[i][0]) < Number(twodarr[j][0])){
+                    let temp = twodarr[j];
+                    twodarr[j] = twodarr[i];
+                    twodarr[i] = temp;
+                }
+            }
         }
-        else {
-            return (a[0] > b[0]) ? -1 : 1;
-        }
+        return twodarr;
     }
+
+
 
     finalMessage(){
         return(`Congrats ${this.username}, your score on this run was ${this.score}/15`)
@@ -21,12 +27,14 @@ export class User {
 
         if(this.high_scores.length < 5){
             this.high_scores.push([score, difficulty]);
-            this.high_scores.sort(this.sortFunction);
+            this.high_scores = this.sortFunction(this.high_scores)
         }
         else if(this.high_scores.length == 5){
-            if(this.high_scores[4][0] <  score){
+            console.log(`The score is ${score}`)
+            if(Number(this.high_scores[4][0]) <  Number(score)){
                 this.high_scores[4] = [score,difficulty]
-                this.high_scores.sort(this.sortFunction);
+                this.high_scores = this.sortFunction(this.high_scores)
+                console.log("Sorted")
             }
         }
         sessionStorage.setItem("high_scores", JSON.stringify(this.high_scores));
