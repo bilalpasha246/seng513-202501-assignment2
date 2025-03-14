16f1db9b36072fdae37cf39bd2d4805770641619
bilalpasha246 <94236236+bilalpasha246@users.noreply.github.com>
2025-03-13 18:10:57 -0600

    Updated Username Input validation + counter placeholder
    provides red border if user does not enter a valid message.

--- script.js
+++ script.js
@@ -4,28 +4,44 @@ import { User } from "./user.js"
 document.addEventListener("DOMContentLoaded", async () => {
   const user = new User()
   const submitButton = document.querySelector(".submit");
+  const usernameInput = document.getElementById("username");
   if(submitButton){
     submitButton.addEventListener("click", async () => {
-      const usernameInput = document.getElementById("username");
-
-      if(usernameInput) {
-        sessionStorage.setItem("username", usernameInput.value);
-        window.location.replace("index.html");
+      const usernameValue = usernameInput.value.trim();
+      if (!usernameValue) {
+        usernameInput.style.border = "2px solid red";
+        return; 
       }
+      else{
+        usernameInput.style.border = "";
+      }
+      sessionStorage.setItem("username", usernameInput.value);
+      window.location.replace("index.html");
     })
   }
 if(window.location.pathname.includes("index.html")){ 
+  const questionCount = document.getElementById("question-count")
+  const countDown = document.getElementsByClassName("question")[0];
   const quiz = new Quiz();
-  await new Promise(resolve => setTimeout(resolve, 3000));
+  questionCount.innerHTML = `<b>${quiz.counter}</b> of <b>15</b>`
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  countDown.innerHTML = "3"
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  countDown.innerHTML = "2"
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  countDown.innerHTML = "1"
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
   await quiz.fetchQuestions();
   document.querySelector(".next").addEventListener("click", async () => {
-    if(quiz.score%5 == 0 && quiz.score != 0){
-      await quiz.fetchQuestions();
-    }
     if(quiz.counter == 16){
       sessionStorage.setItem("quiz_score", quiz.score);
       sessionStorage.setItem("quiz_diff", quiz.getlastdifficulty())
       window.location.replace("game-over.html");
+    }
+    questionCount.innerHTML = `<b>${quiz.counter}</b> of <b>15</b>`
+    if(quiz.score%5 == 0 && quiz.score != 0){
+      await quiz.fetchQuestions();
     }
     quiz.displayQuestion();
   });
